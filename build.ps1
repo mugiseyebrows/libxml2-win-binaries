8fbb55d14727fa354422c05148b93ea2127c9185
Doronin Stanislav <mugisbrows@gmail.com>
2022-12-09 23:06:14 +0300

    test

--- build.ps1
+++ build.ps1
@@ -19,7 +19,7 @@ If($vs2008) {
     Import-VisualStudioVars -VisualStudioVersion "90" -Architecture $vcvarsarch
 } Else {
     $vcvarsarch = If($x64) { "x86_amd64" } ElseIf ($arm64) { "x86_arm64" } Else { "32" }
-    cmd.exe /c "call `"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars$vcvarsarch.bat`" && set > %temp%\vcvars$vcvarsarch.txt"
+    cmd.exe /c "call `"C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars$vcvarsarch.bat`" && set > %temp%\vcvars$vcvarsarch.txt"
     Get-Content "$env:temp\vcvars$vcvarsarch.txt" | Foreach-Object {
         if ($_ -match "^(.*?)=(.*)$") {
             Set-Content "env:\$($matches[1])" $matches[2]
